d2a1cb4ba2b5991a793c9bf299502b1fcef41122
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-01-29 22:56:20 +0530

    feat(spec): added generics to WindowFactory

--- specification/src/main/java/org/dtomics/gameengine/specification/window/WindowFactory.java
+++ specification/src/main/java/org/dtomics/gameengine/specification/window/WindowFactory.java
@@ -7,19 +7,32 @@ import java.util.Optional;
 /**
  * WindowFactory
  */
-public abstract class WindowFactory {
+public abstract class WindowFactory<W extends Window> {
 
-    protected abstract <T extends Window> T create(WindowConfig config);
+    protected abstract W create(WindowConfig config);
 
-    private static final Map<Class<? extends Window>, WindowFactory> windowFactoryCache = new HashMap<>();
+    private final Class<W> clz;
 
-    protected WindowFactory(Class<? extends Window> clz) {
+    protected WindowFactory(Class<W> clz) {
+        this.clz = clz;
         windowFactoryCache.put(clz, this);
     }
 
+    protected boolean supports(final Class<? extends Window> clz) {
+        return this.clz.isAssignableFrom(clz);
+    }
+
+    private static final Map<Class<? extends Window>, WindowFactory<? extends Window>> windowFactoryCache = new HashMap<>();
+
+    public static <T extends Window> void register(final Class<T> clz, WindowFactory<T> factory) {
+        windowFactoryCache.put(clz, factory);
+    }
+
+    @SuppressWarnings({ "unchecked" })
     public static <T extends Window> Optional<T> create(final Class<T> clz, final WindowConfig config) {
         return Optional.ofNullable(windowFactoryCache.get(clz))
-            .map(factory -> factory.create(config));
+                .filter(factory -> factory.supports(clz))
+                .map(factory -> (T) factory.create(config));
     }
 
 }
